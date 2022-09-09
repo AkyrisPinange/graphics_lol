41edc0b296aeade5fab8577f41ca9cca8ee70f02
AkyrisPinange <akyris.2903@gmail.com>
2022-09-09 12:40:08 -0300

    CRUD SUMONNER

--- src/controllers/SumonnerController.ts
+++ src/controllers/SumonnerController.ts
@@ -1,27 +1,66 @@
 import { Request, Response } from "express";
-import { sumonnerRepository } from '../repositories/SumonnerRepository';
+import { sumonnerRepository } from "../repositories/SumonnerRepository";
+import { AppDataSource } from "../data-source";
+import { Sumonner } from "../entities/Sumonner";
+import { ILike } from "typeorm";
 
+export default class SumonnerController {
+  async create(req: Request, res: Response) {
+    const { puuid, name } = req.body;
 
+    try {
+      const userExists = await sumonnerRepository.findOne({
+        where: {
+          puuid: puuid,
+        },
+      });
 
-export  default class SumonnerController{
+      if (userExists?.puuid != null) {
+        return res
+          .status(200)
+          .json(`O invocador ${userExists.name} já está cadastrado!`);
+      }
 
-    async create(req: Request, res: Response){
-        const {puuid, name} = req.body;  
+      const newSumonner = sumonnerRepository.create({
+        name,
+        puuid,
+      });
 
-        try {
-            const newSumonner = sumonnerRepository.create({
-                name,
-                puuid
-            })
-            
-            await sumonnerRepository.save(newSumonner);
+      await sumonnerRepository.save(newSumonner);
 
-            return res.status(201).json(newSumonner)
-            
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({error})
-        }
-
+      return res.status(201).json(newSumonner);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error });
     }
+  }
+  async update(req: Request, res: Response) {
+    const { puuid, name } = req.body;
+
+    const userExists = await sumonnerRepository.findOne({
+      where: {
+        puuid: puuid,
+      },
+    });
+
+    if (userExists?.puuid === null) {
+      return res.status(200).json(`O invocador ${userExists.name} não existe!`);
+    }
+
+    const updateSumonner = await AppDataSource.createQueryBuilder()
+      .update(Sumonner)
+      .set({ name: name })
+      .where("puuid = :puuid", { puuid: puuid })
+      .execute();
+
+    return res.status(201).json("Atualiza Com sucesso!");
+  }
+  async list(req: Request, res: Response) {
+    const { name } = req.body;
+
+    const sumonners = await sumonnerRepository.findBy({
+        name: ILike(`%${name}%`)
+    })
+    return res.status(200).json(sumonners);
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,14 @@ import SumonnerController from './controllers/SumonnerController';
 import GetMatcheByPuuidController from './controllers/GetMatcheByPuuidController';
 
 const routes = Router();
+    
+/***************************************************************/
+/*                         SUMONNER                            */   
+routes.post('/sumonnerSave', new SumonnerController().create)
+routes.put('/sumonnerUpdate', new SumonnerController().update)
+routes.get('/sumonnerList', new SumonnerController().list)
+/**************************************************************/
 
-routes.post('/sumonner', new SumonnerController().create)
 routes.post('/infoMatchCreate', new InfoMatchController().create)
 // routes.post('/infoMatchList', new InfoMatchController().List)
 routes.post('/getMatchBypuuid', new GetMatcheByPuuidController().List)
